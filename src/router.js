a1cbda984545565fda19178eeefe1bef2448a41b
sanaz <sanazjavadi72@yahoo.com>
2019-08-23 13:56:41 -0400

    creating sign in & aign up route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import signin from './views/signin'
+import signup from './views/signup'
 
 
 Vue.use(Router);
@@ -22,6 +24,16 @@ export default new Router({
               path:'/about',
               name: 'about',
               component: About,
+       },
+       {
+              path:'/signin',
+              name:'signin',
+              component:signin,
+       },
+       {
+              path:'signup',
+              name:'signup',
+              component:signup,
        }
 	] 
 })
---
+++ src/views/signin.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        this is signin page
+        </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
