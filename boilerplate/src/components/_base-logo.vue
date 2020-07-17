86931b606f339d481dd4bb64b8f955fa86e6e60b
sanazjavadi <sanazjavadi72@yahoo.com>
2020-07-17 10:37:36 -0400

    logo component

--- boilerplate/src/components/_base-logo.vue
+++ boilerplate/src/components/_base-logo.vue
@@ -2,13 +2,44 @@
 import logosvg from '@components/svg/logo-svg.vue'
 export default {
   components: { logosvg },
+  props: {
+    height: {
+      type: Number,
+      default: () => {
+        return 80
+      },
+    },
+    width: {
+      type: Number,
+      default: () => {
+        return 80
+      },
+    },
+  },
 }
 </script>
 
 <template>
-  <logosvg />
+  <div class="wrapper">
+    <logosvg
+      class="logo"
+      :style="{ height: height + 'px', width: width + 'px' }"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @import '@design';
+.wrapper {
+  position: relative;
+  display: inline-block;
+  vertical-align: middle;
+  padding-bottom: 100%;
+  .logo {
+    position: absolute;
+    display: inline-block;
+    top: 0;
+    left: 0;
+  }
+}
 </style>
--- boilerplate/src/router/views/home.vue
+++ boilerplate/src/router/views/home.vue
@@ -14,9 +14,7 @@ export default {
   <div class="container-fluid">
     <div class="row">
       <div class="col-lg-12">
-        <div class="wrapper">
-          <BaseLogo class="logo"> </BaseLogo>
-        </div>
+        <baseLogo />
       </div>
     </div>
   </div>
@@ -24,18 +22,4 @@ export default {
 
 <style lang="scss" scoped>
 @import '@design';
-.wrapper {
-  position: relative;
-  display: inline-block;
-  vertical-align: middle;
-  padding-bottom: 100%;
-  .logo {
-    position: absolute;
-    display: inline-block;
-    top: 0;
-    left: 0;
-    width: 100px;
-    height: 100px;
-  }
-}
 </style>
